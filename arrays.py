6622c2d6a15a7569453fdb92f7542aae4f401125
prashantchoudhary07 <prashantchoudharyi13338b@gmail.com>
2026-06-14 09:15:36 +0530

    arrays.py

---
+++ arrays.py
@@ -0,0 +1,42 @@
+fruits = ["apple", "banana", "orange"]
+
+print(fruits[0])   
+print(fruits[1])
+
+
+numbers = [1,2,3,4,5,6,7,8]
+
+numbers.append(9)      
+numbers.remove(8)      
+print(len(numbers))     
+print(numbers)
+
+#indexing
+
+fruits = ["apple", "banana", "mango", "orange"]
+
+print(fruits[0])   
+print(fruits[2])   
+
+fruits = ["apple", "banana", "mango", "orange"]
+
+print(fruits[-1])  
+print(fruits[-2])  
+
+name = "Prashant"
+
+print(name[0])   
+print(name[3]) 
+
+name = "Prashant"
+
+print(name[0:4])
+
+numbers = [10, 20, 30, 40, 50, 60]
+
+print(numbers[1:5])
+print(numbers[0:6:2])
+
+name = "Python"
+
+print(name[::-1])
