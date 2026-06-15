f857cdc09078e58723236ffdbf456203bafe3eca
prashantchoudhary07 <prashantchoudharyi13338b@gmail.com>
2026-06-15 12:32:21 +0530

    array operations.py

---
+++ array operations.py
@@ -0,0 +1,15 @@
+arr = [1,2,4,5,6,78,9]
+print(arr[1])
+
+arr.append(34)
+arr.insert(1,100)
+print(arr)
+
+arr.remove(100)
+arr.pop()
+print(arr)
+
+print(8 in arr)
+print(100 in arr)
+print(78 in arr)
+
